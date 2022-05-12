d635fc67b629130c1cadfb6c877736c68d6f3967
Uehara Shoya <kd1297534@st.kobedenshi.ac.jp>
2022-05-12 11:07:38 +0900

    質問投稿画面のcssとhtmlの修正#45

--- question_write.php
+++ question_write.php
@@ -1,37 +1,103 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="stylesheet" href="./css/question_write.css">
-  <title>質問フォーム</title>
+    <meta charset="UTF-8">
+    <meta name="description" content=>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/question_write.css">
+    <title>質問投稿画面</title>
 </head>
-  <body>
-    <form action="" method="post">
+<body>
+    <div class="oya">
+        <div class="div_1">
+            <div class="resetbtn_div">
+                <a class="a_btn" href="">リセット</a>
+            </div>
 
-    <p>質問投稿</p>
-   
-    <span class="title-span">質問タイトル</span>
+            <div class="btn_div">
+                <a class="btn" href="">投稿</a>
+            </div>
+        </div>
 
-    <div>
-      <input class="textbox" type="text" name="name">
+        <div class="character_div">
+            <div class="title_div">
+                <span>タイトル</span>
+            </div>
+            <div class="trauble_div">
+                <span>困っている度</span>
+            </div>
+        </div>
 
-        <select class="select">
-          <option hidden>優先度</option>
-          <option value="high">高</option>
-          <option value="middle">中</option>
-          <option value="low">低</option>
-        </select><br>
+        <div class="ko_div">
+            <div class="ko1">
+                <form method="post" action="">
+                    <div class="div_2">
+                        <span class="title_span">
+                            <input class="titletext" type="text" id="title" name="title">
+                        </span>
 
-        <span class="text">名前</span>
-      <input class="textboxname" type="text" name="name">
+                        <span class="trauble_span">
+                            <select class="cp_ipselect" id="pulldown">
+                            <option hidden>困っている</option>
+                            <option value="1">高</option>
+                            <option value="2">中</option>
+                            <option value="3">低</option>
+                            </select>
+                        </span>
+                    </div>
 
-      <span class="text">質問詳細</span>
-      <textarea class="comment" name="comment" cols="30" rows="5"></textarea><br>
+                    <div class="div_3">
+                        <label class="message" for="message">質問内容</label>
+                        <textarea class="messagetext" id="message" name="message"></textarea>
+                    </div>
+                    <div class="img">
+                        <input type="file" name="avatar">
+                    </div>
+                </form>
+            </div>
 
-      <button class="btn">投稿</button>
+            <div class="ko2">
+                <div class="tag_text_div">
+                    <span class="tag_text">
+                        タグ選択
+                    </span>
+                </div>
+
+                <div class="search_div">
+                    <input class="box_search" type="search" name="search" placeholder="キーワードを入力">
+                </div>
+
+                <div class="checkbox">
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="C言語"><label for="C言語">C言語</label>
+                    </div>
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="Java"><label for="Java">Java</label>
+                    </div>
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="Python"><label for="Python">Python</label>
+                    </div>
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="PHP"><label for="PHP">PHP</label>
+                    </div>
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="HTML"><label for="HTML">HTML</label>
+                    </div>
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="CSS"><label for="CSS">CSS</label>
+                    </div>
+                    <div class="checkbox_div">
+                        <input type="checkbox" id="scales" name="IoT"><label for="IoT">IoT</label>
+                    </div>
+                </div>
+
+                <hr>
+                <div class="selection">
+                    <span>選択済みタグ</span>
+                </div>
+
+            </div>
+        </div>
     </div>
-
-</form>
 </body>
 </html>
