fa0a11b9a38373cc66307538dfe2d128985ab9e1
Terasaki Naoto <kd1289594@st.kobedenshi.ac.jp>
2022-05-12 11:15:21 +0900

    途中経過１/#41

---
+++ article_posting.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>記事投稿画面</title>
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" type="text/css" href="./css/production_write.css">
+    </head>
+
+    <body>
+        <p>タイトル</p>
+            <input class="textbox" type="text" name="title">
+        <p>投稿内容</p>
+            <input class="textarea" type="text" name="toukou" rows=”3″ cols=”40″ wrap=”hard”>
+    </body>
+    
+</html>
