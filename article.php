89b8c52b56e6f0f921e846bdeb18d7a0b134bc06
O-Ricky <ritsuki826@icloud.com>
2022-05-12 13:03:34 +0900

    記事詳細画面の五段階評価のズレの修正#53

--- article.php
+++ article.php
@@ -64,7 +64,7 @@
       この記事に対するコメント
     </p>
   </div>
-  <div class="contents">
+  <div class="star-contents">
     <p>
       <i>閲覧数:256件 評価:4.0</i><span class="star5_rating" data-rate="4"></span>
     </p>
