a53b676f0ca63182426ed3b06dfb720bbb824b3b
Terasaki Naoto <kd1289594@st.kobedenshi.ac.jp>
2022-05-16 09:45:38 +0900

    変更/#41

--- article_posting.php
+++ article_posting.php
@@ -6,14 +6,31 @@
         <meta charset="UTF-8">
         <title>記事投稿画面</title>
         <link rel="stylesheet" href="./css/style.css">
-        <link rel="stylesheet" type="text/css" href="./css/production_write.css">
+        <link rel="stylesheet" type="text/css" href="./css/article_posting.css">
     </head>
 
     <body>
-        <p>タイトル</p>
-            <input class="textbox" type="text" name="title">
-        <p>投稿内容</p>
-            <input class="textarea" type="text" name="toukou" rows=”3″ cols=”40″ wrap=”hard”>
+        <!--　リセットボタン　-->
+        <input type="reset" value="リセット" class="riset_button" style="position: relative; top:15px; left:2%;">
+
+        <!--　投稿ボタン　-->
+        <input type="submit" name="submit" value="投稿" class="btn" style="position: relative; top:15px; left:70%;">
+        <br>
+
+        <div class="flex">
+            <p style="position: relative; top:15px; left:3%;"><br><br>タイトル</p>
+                <input class="textbox" type="text" name="title" style="position: relative; top:15px; left:3%;">
+            <p style="position: relative; top:15px; left:3%;"><br>投稿内容</p>
+                <textarea class="textarea" type="text" name="toukou" rows=”18″ cols=”40″ wrap=”hard” style="position: relative; top:15px; left:3%;"></textarea>
+
+            <div class="box1" style="position: relative;">
+        </div>
+
+        <br><br>
+        <input type="file" style="position: relative; top:15px; left:3%;">
+
+        
+
     </body>
     
 </html>
